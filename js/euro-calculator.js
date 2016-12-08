5bb4a1d465dc88f33075fccbbe04b40e6fe1f12e
wdfeww <wdfeww@gmail.com>
2016-12-08 12:30:44 +0100

    functions in euro calculator changed to jquery

--- js/euro-calculator.js
+++ js/euro-calculator.js
@@ -2,33 +2,16 @@
 (function ($, window, document, undefined) {
 
     $.fn.euroCalculator = function () {
-        var currency;
-        var currenclyType = {
-            usd: 0,
-            gbp: 0,
-            aud: 0,
-            czk: 0,
-            cad: 0,
-            chf: 0,
-            getRate: function(response){
-                     var currency = JSON.parse(response);
-            currenclyType.usd = currency.query.results.rate[0].Rate;
-            currenclyType.gbp = currency.query.results.rate[1].Rate;
-            currenclyType.aud = currency.query.results.rate[2].Rate;
-            currenclyType.czk = currency.query.results.rate[3].Rate;
-            currenclyType.cad = currency.query.results.rate[4].Rate;
-            currenclyType.chf = currency.query.results.rate[5].Rate;
-            }
-        };
-        var xmlhttp = new XMLHttpRequest();
-        var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22EURUSD%22%2C%20%22EURGBP%22%20%2C%20%22EURAUD%22%20%2C%20%22EURCZK%22%20%2C%20%22EURCAD%22%20%2C%20%22EURCHF%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                currenclyType.getRate(this.responseText);
-            }
-        }
+        var currenclyType = {};
+           
+         $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22EURUSD%22%2C%20%22EURGBP%22%20%2C%20%22EURAUD%22%20%2C%20%22EURCZK%22%20%2C%20%22EURCAD%22%20%2C%20%22EURCHF%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", function (data) {
+             currenclyType["usd"] = data.query.results.rate[0].Rate;
+             currenclyType["gbp"] = data.query.results.rate[1].Rate;
+             currenclyType["aud"] = data.query.results.rate[2].Rate;
+             currenclyType["czk"] = data.query.results.rate[3].Rate;
+             currenclyType["cad"] = data.query.results.rate[4].Rate;
+             currenclyType["chf"] = data.query.results.rate[5].Rate;
+        });
 
 
         $('<div />', {
@@ -101,7 +84,6 @@
 
         var convert = function () {
             var output = $("#calculated");
-
             var value = ($("#eur-Input").val())
             var type = ($('input[name=curren]:checked', '#cur-Type').attr("id"));
             if (!$.isNumeric(value) || (value.indexOf('e') > -1)) {
