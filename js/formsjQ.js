9b4d693720df221adecb6a0059441fa54f983e70
R0mulus <jozef.balint@akademiasovy.sk>
2017-01-11 12:48:39 +0100

    plus, minus and e no longer permitted in amounts

--- js/formsjQ.js
+++ js/formsjQ.js
@@ -261,6 +261,14 @@ $(document).ready(function () {
             initChart(allData, dataIndex, deletedAmount);
         });
 
+        $('#income-amount, #payment-amount').on('keypress', function (event) {
+            var regex = new RegExp("^[.0-9]+$");
+            var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+            if (!regex.test(key)) {
+                event.preventDefault();
+                return false;
+            }
+        });
 
     })(jQuery);
 
